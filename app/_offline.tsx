9c6d594fd0a352e62c91578c04aa718c6c4a0220
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-03-24 07:38:13 +0500

    trying PWA

---
+++ app/_offline.tsx
@@ -0,0 +1,3 @@
+export default function Offline() {
+  return <div>_offline</div>;
+}
--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  // disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+});
+
+module.exports = withPWA({
   experimental: {
     appDir: true,
   },
-}
-
-module.exports = nextConfig
+});
